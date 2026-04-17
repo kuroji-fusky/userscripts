37b4debd3b53287153015f305c8f560b0c6672e6
Kuro <hello@kurojifusky.com>
2026-04-17 12:03:13 +0800

    render toolbar
    - added sets to track posts
    - added playlistrenderer and refactored

--- youtube/communityPostWatcher.js
+++ youtube/communityPostWatcher.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name         YTCommunityPost++
-// @version      2026-04-12
+// @version      2026-04-17
 // @description  Extends additional utilities to community posts on YouTube
 // @author       Kuroji Fusky
 // @match        https://www.youtube.com/*
@@ -21,9 +21,18 @@ const extractImageUrl = (backstageImages) => {
   return optimizedImg.replace(/=s\d+.*/, "=s0")
 }
 
+const ATTACHMENT = {
+  img: "backstageImageRenderer",
+  multiImg: "postMultiImageRenderer",
+  video: "videoRenderer",
+  playlist: "playlistRenderer",
+  poll: "pollRenderer"
+}
+
 const parsePollAttachment = (pollData) => {
-  const _choices = pollData.choices
   let totalVotes = 0
+
+  const _choices = pollData.choices
   const firstChoiceItem = _choices[0]
 
   if (firstChoiceItem.numVotes) {
@@ -64,23 +73,25 @@ const mapPostAttachments = (attachmentData) => {
 
   const attachmentKey = hasAttachment ? Object.keys(attachmentData)[0] : ""
   if (!hasAttachment) return null
-
-  let attachmentRenderer;
+  let attachmentRenderer = null
 
   switch (attachmentKey) {
-    case "pollRenderer":
+    case ATTACHMENT["poll"]:
       attachmentRenderer = parsePollAttachment(attachmentData.pollRenderer)
       break
-    case "backstageImageRenderer":
+    case ATTACHMENT["img"]:
       attachmentRenderer = extractImageUrl(attachmentData.backstageImageRenderer)
       break
-    case "postMultiImageRenderer":
+    case ATTACHMENT["multiImg"]:
       attachmentRenderer = attachmentData.postMultiImageRenderer.images.map((image) => {
         return extractImageUrl(image.backstageImageRenderer)
       })
       break
-    case "videoRenderer":
+    case ATTACHMENT["video"]:
       attachmentRenderer = "VIDEO"
+      break
+    case ATTACHMENT["playlist"]:
+      attachmentRenderer = "THERE'S PLAYLISTS"
       break
     default:
       console.error(`Unknown key: ${attachmentKey}`)
@@ -92,6 +103,81 @@ const mapPostAttachments = (attachmentData) => {
   }
 }
 
+// --------------------------------------
+// UI
+// 
+class KuroComponent {
+  constructor(component_id, tag, attrs) {
+    this.__host_node = KuroComponent.createElement(tag, {
+      ...Object.fromEntries([[`data-${component_id}`, ""]]),
+      ...attrs
+    })
+
+    Object.assign(this.__host_node, {
+      [Symbol.for("__kuroComponent")]: component_id
+    })
+
+    this._shadowNode = this.__host_node.attachShadow({ mode: "open" })
+  }
+
+  static createElement(tag, attrs) {
+    const el = document.createElement(tag ?? "div")
+
+    if (!attrs) return el
+
+    for (const [attr_key, attr_val] of Object.entries(attrs)) {
+      el.setAttribute(attr_key, attr_val)
+    }
+
+    return el
+  }
+
+  style(styleStr) {
+    const orphanStyleSheet = new CSSStyleSheet()
+    orphanStyleSheet.replaceSync(styleStr)
+
+    this._shadowNode.adoptedStyleSheets = [orphanStyleSheet]
+  }
+
+  appendChildren(...children) {
+    this._shadowNode.append(...children)
+
+    return this.__host_node
+  }
+}
+
+const renderToolbar = (postType) => {
+  const toolContainer = new KuroComponent("kuro-backstage-lowerthird")
+  toolContainer.style(`:host { 
+    display: block; 
+
+    font-family: Roboto, Arial, sans-serif;
+    color: white;
+    font-size: 1.33rem;
+  }
+  
+  #wrapper {
+    margin-top: 0.8rem;
+    padding-block: 0.75rem;
+    padding-inline: 0.85rem;
+    background-color: green;
+    border-radius: 6px;
+  }`)
+
+  const ctWrapper = KuroComponent.createElement("div", {
+    id: "wrapper"
+  })
+  ctWrapper.append("Toolbar placeholder")
+
+  return toolContainer.appendChildren(ctWrapper)
+}
+
+// --------------------------------------
+// ENTRY
+// 
+window.__kuroTrackedPosts = window.__kuroTrackedPosts || new Set()
+const traccedPosts = window.__kuroTrackedPosts
+
 const getPosts = () => {
   const backstagePostsEl = Array.from(document.querySelectorAll("ytd-backstage-post-thread-renderer"))
 
@@ -102,12 +188,28 @@ const getPosts = () => {
     const isPostShared = !backstagePostRenderer
 
     const postAttachments = mapPostAttachments(backstageData.backstageAttachment)
+    const postId = backstageData.postId
 
+    
+    // skip rendering with those that has no attachments
+    const hasAttachments = postAttachments && (
+      postAttachments._type === "backstageImageRenderer"
+      || postAttachments._type === "postMultiImageRenderer"
+      || postAttachments._type === "pollRenderer"
+    )
+    
+    const commentContainer = post.querySelector("ytd-comment-action-buttons-renderer")
+    if (!traccedPosts.has(postId) && hasAttachments) {
+      traccedPosts.add(postId)
+      commentContainer.parentNode.insertBefore(renderToolbar(postAttachments._type), commentContainer)
+    }
+
+    // for debugging only; remove if everything's implemented
     return {
-      id: backstageData.postId,
+      id: postId,
       isPostShared,
       attachment: postAttachments,
-      postUrl: `https://www.youtube.com/post/${backstageData.postId}`,
+      // postUrl: `https://www.youtube.com/post/${backstageData.postId}`,
       relativePublishDate: extractRunsText(backstageData.publishedTimeText),
       contentText: Object.entries(backstageData.contentText).length !== 0 ? extractRunsText(backstageData.contentText) : null,
 
@@ -117,7 +219,8 @@ const getPosts = () => {
 
   // Just an edge case everytime "yt-navigate-finish" fires
   if (posts.length === 0) return null
-  _debug({ posts })
+
+  _debug({ trackedPosts: traccedPosts })
 
   return posts
 }
@@ -126,6 +229,11 @@ window.addEventListener("yt-navigate-finish", ({ detail }) => {
   const _pageContext = detail.response.page
 
   if (!(_pageContext === "channel" || (isPostURL() && _pageContext === "browse"))) {
+    // perform a clean if user has navigated elsewhere
+    if (traccedPosts.size === 0) {
+      traccedPosts.clear()
+    }
+
     _debug("Returning data for channel related pages only")
     return
   }
@@ -138,9 +246,9 @@ window.addEventListener("yt-action", ({ detail }) => {
 
   // filtering unneeded noise
   if (
-    _actionType === "yt-forward-redux-action-to-live-chat-iframe" ||
-    _actionType === "yt-user-activity" ||
-    _actionType === "yt-store-grafted-ve-action"
+    _actionType === "yt-forward-redux-action-to-live-chat-iframe"
+    || _actionType === "yt-user-activity"
+    || _actionType === "yt-store-grafted-ve-action"
   ) return
 
   if (isPostURL() && _actionType === "yt-window-scrolled") {
